da8d22e037ca0802cc94ba30685e56b374d3c6b9
Cyril Ethan de Guzman <cyril_deguzman@dlsu.edu.ph>
2021-05-28 21:23:47 +0800

    updated app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ hbs.registerPartials(__dirname + `/views/partials`);
 
 dotenv.config();
 port = process.env.PORT;
-hostname = process.env.HOSTNAME;
 url = process.env.DB_URI;
 secret = process.env.SECRET;
 
@@ -34,6 +33,6 @@ app.use(session({
 
 app.use(`/`, routes);
 
-app.listen(port, hostname, function () {
+app.listen(port, function () {
 	
 });
